96313575d8c9c68342682edd24d1ed72e3f57380
Kaveh Vahedipour <kaveh@arangodb.com>
2018-06-13 11:46:24 +0200

    added stop

--- simple-timer-test.cpp
+++ simple-timer-test.cpp
@@ -23,5 +23,17 @@ int main() {
 
   std::cout << "lap[ms]: " << t2.lap().count() << ", elapsed[ms]: "
             << t2.elapsed().count() << std::endl;
+
+  t1.stop();
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  t1.start();
+  std::cout << "lap[ms]: " << t1.lap().count() << ", elapsed[ms]: "
+            << t1.elapsed().count() << std::endl;
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  std::cout << "lap[ms]: " << t1.lap().count() << ", elapsed[ms]: "
+            << t1.elapsed().count() << std::endl;
+
+  std::cout << "lap times:" << t1.laps<std::nano>() << std::endl;
+  
 }
 
--- simple-timer.h
+++ simple-timer.h
@@ -15,31 +15,70 @@ public:
   }
   
   void start() {
-    lap_.push_back(clock::now());
+    auto n = clock::now();
+    auto d = n - stop_;
+    for (auto& i : lap_) {
+      i += d;
+    }
+    lap_.push_back(n);
     running_ = true;
   }
   
-  template<typename T = std::milli, typename U = long> std::chrono::duration<U,T> elapsed() {
+  template<typename T = std::milli, typename U = long>
+  std::chrono::duration<U,T> elapsed() {
     if (running_) {
       auto n = std::chrono::steady_clock::now();
-      lap_.push_back(n);
-      return std::chrono::duration_cast<std::chrono::duration<U,T>>(n - lap_.front());
+      return std::chrono::duration_cast<std::chrono::duration<U,T>>(
+        n - lap_.front());
     } 
     return std::chrono::duration<U,T>(0);
   }
   
-  template<typename T = std::milli, typename U = long> std::chrono::duration<U,T> lap() {
+  template<typename T = std::milli, typename U = long>
+  std::chrono::duration<U,T> lap() {
     if (running_) {
       auto n = std::chrono::steady_clock::now();
-      auto ret = std::chrono::duration_cast<std::chrono::duration<U,T>>(n - lap_.back());
+      auto ret = std::chrono::duration_cast<std::chrono::duration<U,T>>(
+        n - lap_.back());
       lap_.push_back(n);
       return ret;
     } 
     return std::chrono::duration<U,T>(0);
   }
 
+  template<typename T = std::milli, typename U = long>
+  std::vector<std::chrono::duration<U,T>> laps() {
+    std::vector<std::chrono::duration<U,T>> ret;
+    if (lap_.size() > 0) {
+      for (auto i = 1; i < lap_.size(); ++i) {
+        ret.push_back(std::chrono::duration_cast<std::chrono::duration<U,T>>(
+                        lap_[i] - lap_[i-1]));
+      }
+    }
+    return ret;
+  }
+
+  void stop() {
+    stop_ = clock::now();
+    running_ = false;
+  }
+
+  void reset() {
+    lap_.clear();
+    running_ = false;
+  }
+  
 private:
+  clock::time_point stop_;
   std::vector<clock::time_point> lap_;
   bool running_ = false;
 };
 
+template<typename T>
+inline std::ostream& operator<< (std::ostream& os, std::vector<T> const& vt) {
+  os << "[ ";
+  for (auto const& i : vt)
+    os << i.count() << " ";
+  os << "]";
+  return os;
+}
